6bb41f4c473b17d00206bcf03328b799b9c95ebf
ankit <ankitjangid6375@gmail.com>
2024-12-14 09:28:08 -0500

    creating routers or api testing

---
+++ src/middlwares/multer.middlware.js
@@ -0,0 +1,17 @@
+import multer from "multer"  
+
+const storage = multer.diskStorage({
+    discription:function(req, file , cb){
+        cb(null, "./public/temp")
+    },
+    filename:function(req, file, cb){
+        cb(null, file.originalname)
+    }           
+
+})
+
+const upload = multer({storage:storage})
+
+export default multer
+
+
